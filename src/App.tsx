e5d6be028540232fa32714acd1d88ff83d3ee104
dakShh <daksh.dk207@gmail.com>
2024-12-01 09:15:10 -0500

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css';
 import { cn } from './utils/cn';
+
+import './App.css';
+
 function App() {
-    return <div className={cn()}> app</div>;
+    return <div className={cn()}></div>;
 }
 
 export default App;
